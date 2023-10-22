93e26b18089066a9b2a1a30f3f9723043ff875dd
reeseroo <reesewelty@utexas.edu>
2023-10-21 20:28:25 -0500

    Login Beginning

--- functions/Login.py
+++ functions/Login.py
@@ -4,14 +4,11 @@ from Users import mongoDB
 
 
 def login_screen(display, clock):
-    background = pygame.image.load('sprites/background.jpg')
     if mongoDB.userFind("Jenna", "2345") == 1:
         print("true")
 
-    mongoDB.addNewUser()
     while True:
         display.fill((0, 0, 0))
-        display.blit(background, [0, 0])
         pygame.display.set_caption("Login Screen")
 
         for event in pygame.event.get():
